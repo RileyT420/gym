5913f9bfa04cc7ab3def92ecb31a24fc93e33821
Riley Tate <124rtate@frhsd.com>
2023-01-27 08:33:24 -0500

    added number to sales , javascript variables

---
+++ script.js
@@ -0,0 +1,41 @@
+var number = document.getElementById("number")
+var plus = document.getElementById("plus")
+var minus = document.getElementById("minus")
+var numberA = document.getElementById("numberA")
+var plusA = document.getElementById("plusA")
+var minusA = document.getElementById("minusA")
+var numberB = document.getElementById("numberB")
+var plusB = document.getElementById("plusB")
+var minusB = document.getElementById("minusB")
+var numberC = document.getElementById("numberC")
+var plusC = document.getElementById("plusC")
+var minusC = document.getElementById("minusC")
+var numberE = document.getElementById("numberE")
+var plusE = document.getElementById("plusE")
+var minusE = document.getElementById("minusE")
+var numberF = document.getElementById("numberF")
+var plusF = document.getElementById("plusF")
+var minusF = document.getElementById("minusF")
+var numberG = document.getElementById("numberG")
+var plusG = document.getElementById("plusG")
+var minusG = document.getElementById("minusG")
+var numberH = document.getElementById("numberH")
+var plusH = document.getElementById("plusH")
+var minusH = document.getElementById("minusH")
+var numberI = document.getElementById("numberI")
+var plusI = document.getElementById("plusI")
+var minusI = document.getElementById("minusI")
+var numberJ = document.getElementById("numberJ")
+var plusJ = document.getElementById("plusJ")
+var minusJ = document.getElementById("minusJ")
+var numberK = document.getElementById("numberK")
+var plusK = document.getElementById("plusK")
+var minusK = document.getElementById("minusK")
+
+
+plus.addEventListener("click", function(){
+number.innerHTML = "+1";
+});
+minus.addEventListener("click", function(){
+number.innerHTML = "-1";
+});
